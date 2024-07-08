4b26f8ead53edd2ba90ed0f151b85a187dbcd3e4
HEEKWON <higun295@gmail.com>
2024-07-09 01:24:19 +0900

    add setting.py

--- AboutCare/settings.py
+++ AboutCare/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'users',
+    'reservations',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
